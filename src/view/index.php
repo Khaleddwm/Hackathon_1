20ce9196f44031a6517b8c2cad599bba516665a5
khaleddwm <benk.devweb@gmail.com>
2020-04-29 20:34:06 +0200

    Add .json

---
+++ src/view/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/assets/style.css">
+    <title>Movie Trip</title>
+</head>
+<body>
+
+<?php include_once 'nav.php'; ?>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+  <h1 class="display-4">Vos envies de voyages sont mis à mal durant cette période de confinement ?</h1>
+  <img src="https://images.unsplash.com/photo-1567016515344-5e3b0d67bb75?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="card-img-top" alt="...">
+    <p class="lead">Voyagez tranquillement depuis votre salon grace à Movie Trip. Profitez d'une séléction de reportage et de documentaire pour vous faire voyager et rêver.</p>
+    <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Découvrez notre sélection</a>
+  </p>
+  </div>
+</div>
+​
+<?php include_once 'footer.php'; ?>
+
+</body>
+</html>
