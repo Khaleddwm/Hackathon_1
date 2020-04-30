e1b0c6fab4f07ce9be1fbe14ac7b19f90b4caa86
moshtagh <moshtagh.fr@gmail.com>
2020-04-30 13:54:29 +0200

    added style to css

--- src/view/nav.php
+++ src/view/nav.php
@@ -1,28 +1,28 @@
 <!-- header -->
 <header>
 <nav class="navbar navbar-expand-lg">
-  <a class="navbar-brand" href="index.php">MovieTrip</a>
+  <a class="navbar-brand" href="index.php"> MovieTrip </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home</a>
+        <a class="nav-link" href="index.php">HOME</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="trip.php">Trip</span></a>
+        <a class="nav-link" href="trip.php">TOP DESTINATION</span></a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0" action="search.php" method="get">
-      <input class="form-control mr-sm-2"  type="text" name="search" placeholder="Type Title Here" required>
+      <input class="form-control mr-sm-2"  type="text" name="search" placeholder="NOM DE FILM " required>
       <select name="channel" required>
         <option value="movie" selected="selected">Movie
         </option>
         <option value="tv">TV Show
         </option>
       </select>
-      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"> rechercher</button>
+      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit" style="color: lightgray; font-weight: 500; border-color: lightgrey; padding-top: 0;padding-bottom: 0;"> rechercher</button>
     </form>
   </div>
 </nav>
